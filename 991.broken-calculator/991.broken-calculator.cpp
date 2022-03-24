2de03dfd6823306c782fcc5a8671a43084ae63cc
phJuan <w27101994@gmail.com>
2022-03-24 12:08:48 +0800

    Add sol

---
+++ 991.broken-calculator/991.broken-calculator.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=991 lang=cpp
+ *
+ * [991] Broken Calculator
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        
+    }
+};
+// @lc code=end
+
