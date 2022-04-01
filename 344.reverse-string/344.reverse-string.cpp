26c592b04b93c2dec72af75d20f84915e0d56c7d
phJuan <w27101994@gmail.com>
2022-04-01 15:35:36 +0800

    Add sol

---
+++ 344.reverse-string/344.reverse-string.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=344 lang=cpp
+ *
+ * [344] Reverse String
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        for (int i = 0; i < n / 2; i++) {
+            char tmp = s[i];
+            s[i] = s[n-1-i];
+            s[n-1-i] = tmp;
+        }
+    }
+};
+// @lc code=end
+
