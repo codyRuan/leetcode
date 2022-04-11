be7e7c1ea7ff98abf9456aa1bea8ec0371c913a2
phJuan <w27101994@gmail.com>
2022-04-11 11:48:14 +0800

    Add sol

---
+++ 1260.shift-2-d-grid/1260.shift-2-d-grid.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1260 lang=cpp
+ *
+ * [1260] Shift 2D Grid
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> res(m, vector<int>(n));
+        for (int i = 0; i < m; i ++) {
+            for (int j = 0; j < n; j++) {
+                res[(i + (j + k) / n) % m][(j + k) % n] = grid[i][j];
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+
