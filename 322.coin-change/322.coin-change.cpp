3e7109521093be1889895813236086f58ed1f1bc
phJuan <w27101994@gmail.com>
2022-05-21 18:21:00 +0800

    Add sol

---
+++ 322.coin-change/322.coin-change.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=322 lang=cpp
+ *
+ * [322] Coin Change
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1);
+
+        dp[0] = 0;
+        sort(coins.begin(), coins.end());
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = INT_MAX;
+
+            for (auto c : coins) {
+                if (i - c < 0) break;
+                if (dp[i-c] != INT_MAX) 
+                    dp[i] = min(dp[i], dp[i-c] + 1);
+            }
+        }
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
+    }
+};
+// @lc code=end
+
